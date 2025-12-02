f813b789b50642aaf900abfcb481a0d63163f80d
cmchou73 <sm33525@gmail.com>
2025-12-02 12:52:27 -0700

    Fix description retrieval in _desc_value_from_order

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def _first_item(order):
 def _desc_value_from_order(order):
     sku = (_first_item(order).get("ItemSKU") or _first_item(order).get("ItemCustom"))
     # return f"{sku}  (Electric Fireplace)".strip()
-    return f"{sku} ({od_item.get('Description', '')})".strip()
+    return f"{sku} ({OrderItems.get('Description', '')})".strip()
 
 def _sku8_from_order(order):
     sku = (_first_item(order).get("ItemSKU") or _first_item(order).get("ItemCustom"))
