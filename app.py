fb1b2960631f39127781f56d376941501d7cb5b9
cmchou73 <sm33525@gmail.com>
2025-12-02 13:07:24 -0700

    Modify _desc_value_from_order to return formatted description
    Update description retrieval in _desc_value_from_order function.

--- app.py
+++ app.py
@@ -162,8 +162,9 @@ def _first_item(order):
 
 def _desc_value_from_order(order):
     sku = (_first_item(order).get("ItemSKU") or _first_item(order).get("ItemCustom"))
+    dec = (_first_item(order).get("Description")
     #return f"{sku}  (Electric Fireplace)".strip()
-    return str(od_item.get("Description", "")).strip()
+    return f"{dec}".strip()
     
 def _sku8_from_order(order):
     sku = (_first_item(order).get("ItemSKU") or _first_item(order).get("ItemCustom"))
